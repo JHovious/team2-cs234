db533a16cb2e5d93a3f340503482e50e7d2980c1
krispkreme <96608792+krispkreme@users.noreply.github.com>
2024-10-27 13:37:29 -0600

    Add files via upload

---
+++ stage3/ProjectTest.java
@@ -0,0 +1,15 @@
+
+
+/**
+ *
+ * Christian Kurdi
+ */
+public class ProjectTest {
+
+    
+    public static void main(String[] args) {
+        UserMenu user1 = new UserMenu();
+        user1.showOption();
+    }
+    
+}
---
+++ stage3/UserMenu.java
@@ -0,0 +1,58 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * Christian Kurdi
+ */
+public class UserMenu {
+    
+    //Instance Variables
+    private int option;
+    private boolean choice;
+    
+    
+    //Constructor
+    public UserMenu(){
+        option = 9; //Default number to be changed with working program
+        choice = true;
+        
+        
+    }
+    
+    
+    public void showOption(){
+        
+        do{
+            System.out.println("======Hardware Store User Menu======");
+            System.out.println("1. Tickets Menu");
+            System.out.println("2. Inventory Menu");
+            System.out.println("3. Customer Menu");
+            System.out.println("4. Staff Menu");
+            System.out.println("5. Purchase Menu");
+            System.out.println("6. Store Info");
+            System.out.println("7. Exit Program");
+            System.out.print("Enter your choice: ");
+            Scanner sc = new Scanner(System.in);
+            option = sc.nextInt();
+            switch(option){
+                case 1: 
+                    System.out.println("Test complete");
+                    //Tickets();
+                    break;
+//                case 2:
+//                case 3:
+//                case 4:
+//                case 5:
+//                case 6:
+                case 7: System.out.println("Exiting program, goodbye!"); 
+                choice = false; 
+                break;
+                
+                default: 
+                    System.out.println("Incorrect option, please try again.");
+            }
+        } while (choice);    
+    }
+        
+}
