532508f42e8bd480860ab565712afd220daca138
krispkreme <96608792+krispkreme@users.noreply.github.com>
2024-10-27 15:49:22 -0600

    Update and rename ProjectTest.java to ProjectTest1.0.java

--- stage3/ProjectTest1.0.java
+++ stage3/ProjectTest1.0.java
@@ -9,7 +9,10 @@ public class ProjectTest {
     
     public static void main(String[] args) {
         UserMenu user1 = new UserMenu();
-        user1.showOption();
+        Tickets useTicket = new Tickets();
+        user1.showOption(useTicket);
+        //load in ticket database from .txt file here?
     }
+    
     
 }
