15f43f026800131d277a4380e47ae00c5e2110e7
krispkreme <96608792+krispkreme@users.noreply.github.com>
2024-11-03 15:02:23 -0700

    Update TicketDB.java

--- stage3/TicketDB.java
+++ stage3/TicketDB.java
@@ -3,37 +3,53 @@
 /**
  *
  * Christian Kurdi
- * Version 1.1
  * Needs: like everything for changes
+ * Version: 1.2
  */
-import java.util.ArrayList;
+
+import java.util.Scanner;
 
 
 public class TicketDB {
     
     //Instance Variables
-    private int ticketNumber;
+    
     private String ticketMessage, ticketSubject, ticketPriority, ticketRecipient;
+    private Scanner sc;
     
     
     //Constructor
-    public  TicketDB(ArrayList<TicketDB> aTicketList){
-        this.ticketNumber = aTicketList.size() +1;//I'm not sure if this will cause issues
+    public  TicketDB(){
+        
         this.ticketMessage = "Enter a message";
         this.ticketSubject = "Enter the subject";
         this.ticketPriority = "default";
         this.ticketRecipient = "Manager";
+        sc = new Scanner(System.in);
         
     }
     
-    
-    //Method for the processes needed for reading a ticket like asking for ticket number and
-    
-   
-    //Method to get ticket number
-    public int getTicketNumber(){
-        return this.ticketNumber;
+    //Methods to create a new information
+    public void createTicketSubject(){
+        System.out.println("Enter the subject: ");
+        this.ticketSubject = sc.nextLine(); 
     }
+
+    public void createTicketPriority(){
+        System.out.println("Enter the priority 1-3: ");
+        this.ticketPriority = sc.nextLine(); 
+    }
+
+    public void createTicketRecipient(){
+        System.out.println("Enter the recipient: ");
+        this.ticketRecipient = sc.nextLine(); 
+    }
+
+    public void createTicketMessage(){
+        System.out.println("Enter the message: ");
+        this.ticketMessage = sc.nextLine(); 
+    }
+       
     
     //Method to get ticket priority
     public String getTicketPriority(){
