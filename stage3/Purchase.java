3191aa2fb864e7bc36bf9ad4179efbec70b0ddc3
maya-alt <mayafisher100@gmail.com>
2024-11-05 16:57:17 -0700

    Update Purchase.java

--- stage3/Purchase.java
+++ stage3/Purchase.java
@@ -6,6 +6,11 @@ public class Purchase {
     private String itemQuantity;
     private String removeQuantity;
     
+
+    /*
+     * constructor 
+     * @param String itemName, String itemNumber, float price, String itemQuantity, String removeQuantity
+     */
     public Purchase(String itemName, String itemNumber, float price, String itemQuantity, String removeQuantity) {
         this.itemName = itemName;
         this.itemNumber = itemNumber;
@@ -15,10 +20,16 @@ public class Purchase {
 
     }
 
+    /*
+     * Returns the name of the item.
+     * @return String itemName
+     */
     public String getItem() {
         return itemName;
     }
-
+    /*
+     * Prints the details of the item, including the item name, item number, price, and quantity.
+     */
     public void showItem() {
         System.out.println("Item Name: " + itemName);
         System.out.println("Item Number: " + itemNumber);
@@ -26,7 +37,9 @@ public class Purchase {
         System.out.println("Quantity: " + itemQuantity);
     }
     /*
-     * idk if i did this right.
+     * calculates the subtotal by multiplying the price of the item by the quantity. 
+     * The itemQuantity is converted from a string to a float for the calculation.
+     * @return float subtotal
      */
     public float getSubtotal() {
         float subtotal = price * Float.parseFloat(itemQuantity);
@@ -34,7 +47,10 @@ public class Purchase {
     }
 
     /*
-     * changes the item quantity by the given amount.
+     * attempts to remove a given quantity from the current item quantity.
+     * If the removal quantity is less than or equal to the current quantity then the item quantity is updated.
+     * If the removal quanity is greater than the current quantity an error message is displayed.
+     * @param String removeQuantity
      */
 
     public void removeItem(String removeQuantity) {
@@ -52,7 +68,11 @@ public class Purchase {
     }
 
     /*
-     * updates the item quantity by the given amount 'new Quantity'.
+     * Updates item quantity by adding a given quantity to the current item quantity.
+     * If the result is negative, an error message is displayed and 0 is returned.
+     * Otherwise, the item quantity is updated and returned as a short value
+     * @param String newQuantity
+     * @return short currentQuantity or 0 if update fails
      */
 
     public short updateItemQuantity(String newQuantity) {
