395eff696642624d6170399dbf36e792ef38933a
maya-alt <mayafisher100@gmail.com>
2024-11-09 17:37:37 -0700

    Update Purchase.java

--- stage3/Purchase.java
+++ stage3/Purchase.java
@@ -1,3 +1,6 @@
+/*
+ * Author: maya fisher
+ */
 public class Purchase {
 
     private String itemName;
@@ -5,6 +8,8 @@ public class Purchase {
     private float price;
     private String itemQuantity;
     private String removeQuantity;
+    private Inventory inventory;
+    private Payment payment;
     
 
     /*
@@ -17,8 +22,37 @@ public class Purchase {
         this.price = price;
         this.itemQuantity = itemQuantity;
         this.removeQuantity = removeQuantity;
+        this.inventory =  new Inventory();
+        this.payment = payment;
 
+        // Initialize inventory with dummy data
+        InventoryDB item1 = new InventoryDB();
+        item1.getName();
+        item1.getDetails();
+        item1.getPrice();
+        item1.getQuantity();
+        item1.getLocation();
+
+        inventory.getItems().put(1, item1);   
+    
     }
+
+    public void getInventoryItems(Integer itemNumber) {
+        InventoryDB item = inventory.getItems().get(itemNumber);
+        if(item != null) {
+            System.out.println("Item Name: " + item.getName());
+            System.out.println("Item Details: " + item.getDetails());
+            System.out.println("Item Price: " + item.getPrice());
+            System.out.println("Item Quantity: " + item.getQuantity());
+            System.out.println("Item Location: " + item.getLocation());
+        }
+        else {
+            System.out.println("item not found in inventory");
+        }
+
+       
+    }
+
 
     /*
      * Returns the name of the item.
@@ -41,9 +75,18 @@ public class Purchase {
      * The itemQuantity is converted from a string to a float for the calculation.
      * @return float subtotal
      */
-    public float getSubtotal() {
-        float subtotal = price * Float.parseFloat(itemQuantity);
-        return subtotal;
+    public float getSubtotal(int itemNumber) {
+        InventoryDB item = inventory.getItems().get(itemNumber);
+        if(item != null) {
+            float price = Float.parseFloat(item.getPrice());
+            int itemQuantity = item.getQuantity();
+            float subtotal = price * itemQuantity;
+            return subtotal;
+        }
+        else {
+            System.out.println("Item not found in inventory");
+            return 0;
+        }
     }
 
     /*
@@ -53,18 +96,22 @@ public class Purchase {
      * @param String removeQuantity
      */
 
-    public void removeItem(String removeQuantity) {
-        int removeItemQuantity = Integer.parseInt(removeQuantity);
-        int currentQuantity = Integer.parseInt(itemQuantity);
-        if (removeItemQuantity <= currentQuantity) {
-            int newQuantity = currentQuantity -removeItemQuantity;
-            itemQuantity = String.valueOf(newQuantity);
-            System.out.println("The item has been removed successfully. New quantity:" + itemQuantity);
-        }
+    public void removeItemQuantity(int itemNumber, int removeQuantity) {
+        InventoryDB item = inventory.getItems().get(itemNumber);
+        if(item != null) {
+            int currentQuantity = item.getQuantity();
+            if (removeQuantity <= currentQuantity) {
+                int newQuantity = currentQuantity - removeQuantity;
+                item.setQuantity(newQuantity);
+                System.out.println("The item has been removed successfully. New quantity: " + newQuantity);
+            } else {
+                System.out.println("Error. Cannot remove more items than available.");
+            }
+        } else {
+            System.out.println("Item not found in inventory.");
 
-        else {
-            System.out.println("Error. Cannot remove more items than available.");
         }
+        
     }
 
     /*
@@ -75,19 +122,24 @@ public class Purchase {
      * @return short currentQuantity or 0 if update fails
      */
 
-    public short updateItemQuantity(String newQuantity) {
-        int currentQuantity = Integer.parseInt(this.itemQuantity);
+public short updateItemQuantity(int itemNumber, String newQuantity) {
+    InventoryDB item = inventory.getItems().get(itemNumber);
+    if (item != null) {
+        int currentQuantity = item.getQuantity();
         int quantityChange = Integer.parseInt(newQuantity);
-        
+
         if (currentQuantity + quantityChange < 0) {
             System.out.println("Error. Cannot update quantity to a negative value.");
             return 0;
         } else {
             currentQuantity += quantityChange;
-            this.itemQuantity = String.valueOf(currentQuantity);
-            System.out.println("The item quantity has been updated.");
+            item.setQuantity(currentQuantity);
+            System.out.println("The item quantity has been updated. New quantity: " + currentQuantity);
             return (short) currentQuantity;
         }
+    } else {
+        System.out.println("Item not found in inventory.");
+        return 0;
     }
-
+}
 }
