b07668215345f577208f161de0f118d4cf7d181f
krispkreme <96608792+krispkreme@users.noreply.github.com>
2024-12-11 18:04:39 -0700

    Update TicketsMenu.java

--- stage4/TicketsMenu.java
+++ stage4/TicketsMenu.java
@@ -262,30 +262,12 @@ public class TicketsMenu extends javax.swing.JFrame {
     }                                           
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {                                  
-        try{
-            //Gets data from file
-            BufferedReader reader = new BufferedReader(new FileReader("ticketdata.csv"));
-            ArrayList<String> tableInfo = new ArrayList<>();
-            String line;
-            
-            while ((line = reader.readLine()) != null){
-                tableInfo.add(line);
-            }
-            reader.close();
-            //Put data in table
-            DefaultTableModel model = (DefaultTableModel)tblTicket.getModel();
-            for(String rowInfo : tableInfo){
-                String[] row = rowInfo.split(",");
-
-                TicketDB ticket = new TicketDB(Integer.valueOf(row[0]), row[1], row[2], row[3], row[4]);
-                tickets.put(Integer.valueOf(row[0]), ticket);
-                String[] newRow = new String[] {row[0], row[1], row[2], row[3], row[4]};
+        DefaultTableModel model = (DefaultTableModel)tblTicket.getModel();
+            for(int key : tickets.keySet()){
+                
+                String[] newRow = new String[] {String.valueOf(key), tickets.get(key).getTicketRecipient(), tickets.get(key).getTicketSubject(), tickets.get(key).getTicketPriority()};
                 model.addRow(newRow);
             }
-            
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(this, "Data was not loaded correctly");
-        }
     }                                 
 
     private void clearFields(){
