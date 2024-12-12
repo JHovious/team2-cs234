9d31023dbaa9b836126cf83a1c93e855c8acda02
krispkreme <96608792+krispkreme@users.noreply.github.com>
2024-12-11 17:54:40 -0700

    Update temp.java

--- stage4/temp.java
+++ stage4/temp.java
@@ -1,5 +1,24 @@
-DefaultTableModel model = (DefaultTableModel)tblTicket.getModel();
-        for(int key : tickets.keySet()){
-            String[] newRow = new String[] {String.valueOf(key), tickets.get(key).getTicketRecipient(), tickets.get(key).getTicketSubject(), tickets.get(key).getTicketPriority()};
-            model.addRow(newRow);
+try{
+            //Gets data from file
+            BufferedReader reader = new BufferedReader(new FileReader("ticketdata.csv"));
+            ArrayList<String> tableInfo = new ArrayList<>();
+            String line;
+            
+            while ((line = reader.readLine()) != null){
+                tableInfo.add(line);
+            }
+            reader.close();
+            //Put data in table
+            DefaultTableModel model = (DefaultTableModel)tblTicket.getModel();
+            for(String rowInfo : tableInfo){
+                String[] row = rowInfo.split(",");
+
+                TicketDB ticket = new TicketDB(Integer.valueOf(row[0]), row[1], row[2], row[3], row[4]);
+                tickets.put(Integer.valueOf(row[0]), ticket);
+                String[] newRow = new String[] {row[0], row[1], row[2], row[3], row[4]};
+                model.addRow(newRow);
+            }
+            
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(this, "Data was not loaded correctly");
         }
